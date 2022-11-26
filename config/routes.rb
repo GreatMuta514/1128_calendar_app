ab805e19c009fcbfe6192dc3ad71fbcbc853ccb3
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-11-26 15:25:59 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  #ログイン機能
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  #ユーザー機能
   resources :users, only: [:new, :create, :index]
+
+  #静的なページ
   get 'static_pages/top'
   root 'static_pages#top'
 end
